f2d465baa48f0be076513d2ba786ec90ab6b995e
Tim Allen <tim@noblesamurai.com>
2018-06-14 15:20:52 +1000

    Initial cut.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const ffmpeg = require('fluent-ffmpeg');
+const request = require('request');
+const { PassThrough } = require('stream');
+
+module.exports = async function (url) {
+  return new Promise(function (resolve, reject) {
+    try {
+      const stream = request.get(url).pipe(new PassThrough());
+
+      stream.once('end', function () {
+        console.log('shouldn\'t get to end!');
+      });
+
+      ffmpeg()
+        .once('error', reject)
+        .input(stream)
+        .ffprobe(0, function (err, metadata) {
+          if (err) {
+            reject(err);
+          }
+          resolve(metadata);
+        });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
---
+++ test/index.js
@@ -0,0 +1,9 @@
+const remoteffprobe = require('..');
+
+describe('remote-ffprobe', function () {
+  it('works', async function () {
+    this.timeout(10000);
+    const metadata = await remoteffprobe('https://s3.amazonaws.com/contentsamurai.com/users/1394472/videos/149464/video.mp4?AWSAccessKeyId=***REMOVED***&Expires=1529485966&Signature=q4LZwArvNtL%2FsuY1rmJOi%2FpuDZ0%3D');
+    console.log(metadata.streams[1].width, metadata.streams[1].height);
+  });
+});
