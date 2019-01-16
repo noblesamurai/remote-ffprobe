90515a5d24c0418f08c43dd2c915e4da0ba5c789
Andrew Harris <andrew@noblesamurai.com>
2019-01-16 18:52:13 +1100

    fallback to download ffprobe if missing info

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const ffmpeg = require('fluent-ffmpeg');
-const request = require('request');
-const { PassThrough } = require('stream');
 const ffprobe = require('ffprobe-static');
+const fs = require('fs');
+const got = require('got');
+const path = require('path');
+const tempy = require('tempy');
 ffmpeg.setFfprobePath(ffprobe.path);
 
 /**
@@ -11,30 +13,51 @@ ffmpeg.setFfprobePath(ffprobe.path);
 /**
  * @async
  * @param {string} url
- * @param {number} timeout
- * @returns {object} the ffprobe metadatra
+ * @param {object} opts request options (ie. { timeout: 1500 }).
+ * @returns {object} the ffprobe metadata
  */
-module.exports = async function (url, timeout = 1500) {
-  return new Promise(function (resolve, reject) {
-    try {
-      const buffer = new PassThrough();
-      const stream = request.get(url, { timeout })
-        .on('error', (err) => {
-          buffer.end(); // close the buffer stream so everything finishes.
-          reject(err);
-        })
-        .pipe(buffer);
-
-      ffmpeg()
-        .on('error', reject)
-        .input(stream)
-        // .ffprobe(streamIndex, callback)
-        .ffprobe(0, function (err, metadata) {
-          if (err) return reject(err);
-          resolve(metadata);
-        });
-    } catch (err) {
-      reject(err);
-    }
-  });
+module.exports = async function probe (url, opts = {}) {
+  const stream = got.stream(url, opts);
+  const input = opts.download ? await downloadStream(url, stream) : stream;
+  try {
+    const data = await _probe(input);
+    if (opts.download || (data && data.streams[0] && data.streams[0].profile !== 'unknown')) return data;
+    throw new Error('streaming probe failed, download and try again.');
+  } catch (err) {
+    return probe(url, { ...opts, download: true });
+  }
 };
+
+/**
+ * execute the actual ffprobe call.
+ *
+ * @async
+ * @param {string|stream} input either a stream or a filename.
+ * @returns {object} the ffprobe metadata
+ */
+async function _probe (input) {
+  return new Promise((resolve, reject) => {
+    ffmpeg()
+      .on('error', reject)
+      .input(input)
+      .ffprobe(0, (err, data) => err ? reject(err) : resolve(data));
+  });
+}
+
+/**
+ * download a stream to a temp file.
+ *
+ * @async
+ * @param {stream} stream
+ * @returns {string} filename
+ */
+async function downloadStream (url, stream) {
+  const filename = tempy.file({ extension: path.extname(url) });
+  await new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(filename);
+    file.on('finish', resolve);
+    file.on('error', reject);
+    stream.pipe(file);
+  });
+  return filename;
+}
