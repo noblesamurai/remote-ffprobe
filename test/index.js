1b38af6b8fde46decaea26ab4b7a20393447bccc
Tim Allen <tim@noblesamurai.com>
2020-10-07 11:49:46 +1100

    fix tests.

--- test/index.js
+++ test/index.js
@@ -1,14 +1,16 @@
 const chai = require('chai');
-chai.use(require('chai-as-promised'));
 const expect = chai.expect;
+const ffprobePath = require('ffprobe-static').path;
 const nock = require('nock');
 const path = require('path');
 const remoteffprobe = require('..');
 
+chai.use(require('chai-as-promised'));
+
 describe('remote-ffprobe', function () {
   it('gives ffprobe info for a streamable video', async function () {
     nock('https://testing.com').get('/streamable.mp4').replyWithFile(200, path.resolve(__dirname, 'streamable.mp4'));
-    const info = await remoteffprobe('https://testing.com/streamable.mp4');
+    const info = await remoteffprobe('https://testing.com/streamable.mp4', { ffprobePath });
 
     expect(info).to.be.an('object');
     expect(info).to.have.all.keys('streams', 'format', 'chapters');
@@ -17,7 +19,7 @@ describe('remote-ffprobe', function () {
 
   it('gives ffprobe info for a non-streamable video', async function () {
     nock('https://testing.com').get('/video.mp4').twice().replyWithFile(200, path.resolve(__dirname, 'video.mp4'));
-    const info = await remoteffprobe('https://testing.com/video.mp4');
+    const info = await remoteffprobe('https://testing.com/video.mp4', { ffprobePath });
 
     expect(info).to.be.an('object');
     expect(info).to.have.all.keys('streams', 'format', 'chapters');
@@ -26,13 +28,13 @@ describe('remote-ffprobe', function () {
 
   it('errors cleanly on 404', async function () {
     nock('https://testing.com').get('/404.mp4').reply(404);
-    const probe = remoteffprobe('https://testing.com/404.mp4');
+    const probe = remoteffprobe('https://testing.com/404.mp4', { ffprobePath });
     await expect(probe).to.eventually.be.rejectedWith(Error);
   });
 
   it('should cleanly fail on ETIMEDOUT', async function () {
     nock('https://testing.com').get('/timeout.mp4').delayConnection(1000).reply(500);
-    const probe = remoteffprobe('https://testing.com/timeout.mp4', { timeout: 100 });
+    const probe = remoteffprobe('https://testing.com/timeout.mp4', { timeout: 100, FfprobePath });
     await expect(probe).to.eventually.be.rejectedWith(Error);
   });
   it('should fail cleanly when it\'s actually not a video', async function () {
@@ -40,7 +42,7 @@ describe('remote-ffprobe', function () {
       .get('/somepage.html')
       .times(Infinity)
       .replyWithFile(200, require.resolve('./fixtures/s3.amazonaws.com.html'));
-    const probe = remoteffprobe('https://testing.com/somepage.html');
+    const probe = remoteffprobe('https://testing.com/somepage.html', { ffprobePath });
     await expect(probe).to.eventually.be.rejectedWith(Error);
     nock.cleanAll();
   });
